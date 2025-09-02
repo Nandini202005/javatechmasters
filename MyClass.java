5bdd6b34450a757da7ca6b51f370f4d7a95e0681
Nandini202005 <layanandini20@gmail.com>
2025-09-02 19:37:57 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,33 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    \\The Arithmatic operator
+    System.out.println("Arithmetic operators");
+    int n1=15;
+    int n2=17;
+    int add=n1+n2;
+    int sub=n1-n2;
+    int mult=n1*n2l;
+    int div=n1/n2;
+    int mod=n1%n2;
+    System.out.println("The Sum of the 15 and 17 is :"+ add);
+    System.out.println("The sub of the 15 and 17 is:"- sub);
+    System.out.println("The sub of the 15 and 17 is:"* mult);
+    System.out.println("The sub of the 15 and 17 is:"/ div);
+    System.out.println("The sub of the 15 and 17 is:"% mod);
+    
+  }
+}
