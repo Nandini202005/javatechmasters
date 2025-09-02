40718d97abe5b8dd950967b5a136f211e24bcf4d
Nandini202005 <layanandini20@gmail.com>
2025-09-02 19:43:49 +0530

    Add files via upload

---
+++ MyClass (1).java
@@ -0,0 +1,33 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    // DataTypes
+		System.out.println("There are two types of datatypes.");
+		System.out.println("1. Primtive Datatype.");
+		System.out.println("2. Non-Primitive Datatypes.");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("Deep to 1. Primitive Data type.");
+		System.out.println("The primitive datatype is classified into two types 1. Number and 2.Non-Numeric");
+		System.out.println("(Primitive datatype), 1. Numeric: is classified into TWo types 1. cher, 2.boolean");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("(Primitive datatype), 2. Non-Numeric : is classified into 6 types 1. Byte, 2.Short, 3.int, 4.float, 5.double, 6.long");
+		System.out.println("(Primitive datatype), 1. Numeric: is classified into TWo types 1. cher, 2.boolean");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("2. Non-Primitive Datatype: these are the DataStructure.");
+		System.out.println("they are 1.Array, 2.String, 3.Map, 4.set, 5.Tree, 6.Queue, 7.Stack.");
+
+  }
+}
